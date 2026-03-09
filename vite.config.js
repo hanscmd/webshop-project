2fe7e283f1357fb9107d11dc5a0e6fd7ba339e53
hanscmd <bozidar.djordjevic51@gmail.com>
2026-03-09 23:35:55 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/webshop-project/', // Zameni sa imenom tvog repozitorijuma
+  base: '/', // Zameni sa imenom tvog repozitorijuma
 })
