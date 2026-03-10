ef97ca792a4877e9f8b5202954dc7243c59cae81
hanscmd <bozidar.djordjevic51@gmail.com>
2026-03-10 16:19:06 +0100

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/webshop-project/'   // za GitHub Pages
-    : '/'                   // za lokalni dev i Vercel
+  base: process.env.VERCEL ? '/' : '/webshop-project/'
 })
