666acf33d97fa19a7a0c5d8a3f9a4968655c2846
hanscmd <bozidar.djordjevic51@gmail.com>
2026-03-22 20:30:01 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isVercel = process.env.VERCEL
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/webshop-project/'
+  base: isVercel ? '/' : '/webshop-project/',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
 })
